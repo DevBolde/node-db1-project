46e3ca14f70569370b5509483eb17c416c416333
Devin Bolden <devinbolden6@gmail.com>
2023-08-13 22:24:25 -0500

    wip

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,53 @@
 const router = require('express').Router()
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+  try{
+    res.json({},{})
+  }catch(err){
+    next({status:422, message: 'this is horrible'})
+  }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', async (req, res, next) => {
+  try{
+    res.json({},{})
+
+  }catch(err){
+    next(err)
+  }
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', async (req, res, next) => {
+  try{
+    res.json({},{})
+  }catch(err){
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', async (req, res, next) => {
+  try{
+    res.json({},{})
+  }catch(err){
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', async (req, res, next) => {
+  try{
+    res.json({},{})
+  }catch(err){
+    next(err)
+  }
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use( (err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+ res.status(err.status || 500).json({
+  message:err.message
+ })
 })
+
+
 
 module.exports = router;
